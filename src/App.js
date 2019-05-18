00db7050abe4a5eaa0a23d26cd33192fe01db4a6
DESKTOP-9MGD9U7\MAY <suchanya.roekplain@gmail.com>
2019-05-19 00:24:30 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import pic from './Capture.JPG';
+import TestResume from './components/TestResume'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        data: [],
+    };
+}
+
+componentDidMount() {
+
+    fetch('https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes')
+        .then(results => {
+            return results.json();
+        }).then(data => {
+            let carList = data.results.mgap((item) => {
+                return (
+                    <div key={item.results}>
+                    </div>
+
+                )
+            })
+            this.setState({ data: carList });
+            console.log("state", this.state.data);
+        })
+}
+
+  render(){
+    return (
+      <TestResume  pic={pic}/>
+    )
+  }
 }
 
 export default App;
